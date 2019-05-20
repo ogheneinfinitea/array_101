193fee0be410a107d617f4e6b43bc848655f2810
Samson Ogheneriye Eduotu <ogheneinfinitea>
2019-05-20 14:07:40 +0000

    array

---
+++ arrays.py
@@ -0,0 +1,16 @@
+food = ["RICE","BEANS","FISH"]
+print (food)
+x=food[2]
+print (len(food))
+food.append("MAIZE")
+food.append("plantain")
+food.append("Egg")
+food.append("Water")
+print (food)
+print (len(food))
+food.pop(1)
+print (len(food))
+print (food)
+
+food.pop()
+print (food)
